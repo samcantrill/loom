b511bb340e847941ee594599794701384ca38e17
Sam Cantrill <sam.cantrill@gmail.com>
2026-05-04 16:20:33 +1000

    feat: add plain-data freeze/thaw helpers

--- src/loom/serialization/__init__.py
+++ src/loom/serialization/__init__.py
@@ -7,7 +7,14 @@ from loom.errors import SerializationError
 from loom.serialization.dataclasses import dataclass_from_dict, dataclass_to_dict
 from loom.serialization.errors import DeserializationError, PlainDataError, SchemaVersionError
 from loom.serialization.json import json_dumps_pretty, json_loads, stable_json_bytes, stable_json_dumps
-from loom.serialization.plain import PlainData, ensure_plain_data, is_plain_data, to_plain_data
+from loom.serialization.plain import (
+    PlainData,
+    ensure_plain_data,
+    freeze_plain_data,
+    is_plain_data,
+    thaw_plain_data,
+    to_plain_data,
+)
 from loom.serialization.schema import check_supported_schema, get_schema_version, require_schema_version
 
 __all__ = [
@@ -17,8 +24,10 @@ __all__ = [
     "PlainDataError",
     "SchemaVersionError",
     "is_plain_data",
+    "freeze_plain_data",
     "ensure_plain_data",
     "to_plain_data",
+    "thaw_plain_data",
     "dataclass_to_dict",
     "dataclass_from_dict",
     "stable_json_dumps",
